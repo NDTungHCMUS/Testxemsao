b2f257ced5e1d1f513300506eea12f3b38cdb50c
tungkhongthichcode <94626904+tungkhongthichcode@users.noreply.github.com>
2021-11-19 22:58:29 +0700

    Add files via upload

---
+++ P09.cpp
@@ -0,0 +1,23 @@
+﻿// <21125171>
+// <Nguyễn Đình Tùng>
+// <21CTT1>
+// Test case 1
+// Input: 2
+// Output: 2
+// Test case 2
+// Input: 3 
+// Output: 6 
+// Test case 3
+// Input: 4
+// Output: 24
+#include<iostream>
+#include"MyFunction.h"
+using namespace std;
+int main() {
+	int n;
+	cout << "The purpose of this program is finding the product of the number from 1 to n"<<endl;
+	cout << "Please input n: ";
+	cin >> n;
+	cout << "The product is: " << product(n) << endl;
+	return 0;
+}
